ec6bae250055ffb30a0b86d9a3415ef85d21af2d
GK <jpkutner+github@gmail.com>
2022-07-05 19:20:03 -0500

    created game class

--- game.py
+++ game.py
@@ -1,33 +1,43 @@
 from flask import Flask, render_template, request
 import os.path
 import random
+import uuid
 
-game = Flask(__name__)
+class Game:
+	def __init__(self):
+		f = open(os.path.join("data", "words.txt"))
+		words = f.readlines()
+		w = random.choice(words)
+		self.word = w.strip()
+		self.key = str(uuid.uuid4())
 
-@game.route("/")
+app = Flask(__name__)
+games = {}
+
+@app.route("/")
 def index():
 	return render_template("index.html")
 
-@game.route("/hello/<string:name>/")
+@app.route("/hello/<string:name>/")
 def hello(name):
 	return render_template("hello.html", name=name)
 
-@game.route("/game/new", methods = ['POST'])
+@app.route("/game/new", methods = ['POST'])
 def new_game():
-	f = open(os.path.join("data", "words.txt"))
-	words = f.readlines()
-	w = random.choice(words)
-	word = w.strip()
-	return render_template("game.html", word=word)
+	game = Game()
+	games[game.key] = game
+	print(game.key)
+	return render_template("game.html")
 
-@game.route("/clue", methods = ['POST', 'GET'])
-def give_clue():
+@app.route("/game/<string:key>/clue", methods = ['POST', 'GET'])
+def give_clue(key):
+	game = games[key]
 	if request.method == 'GET':
-		return render_template("clue.html")
+		return render_template("clue.html", word=game.word)
 	elif request.method == 'POST':
 		form_data = request.form
 		print(form_data)
 		return render_template("clue_share.html", clue = form_data["clue"])
 
 if __name__ == "__main__":
-	game.run()
+	app.run()
