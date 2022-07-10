4fdb318397d338e30839767e8bfbe952db01d86b
GK <jpkutner+github@gmail.com>
2022-07-10 14:39:46 -0500

    added random letters for clues

--- game.py
+++ game.py
@@ -4,6 +4,17 @@ import os.path
 import random
 import uuid
 
+EXTRA_LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y"]
+
+def random_letters(word):
+	letters = []
+	while len(letters) < 5:
+		letter = random.choice(EXTRA_LETTERS).upper()
+		if letter not in word:
+			if letter not in letters:
+				letters.append(letter)
+	return letters
+
 class Game:
 	def __init__(self):
 		f = open(os.path.join("data", "words.txt"))
@@ -39,7 +50,8 @@ def new_game():
 def give_clue(key):
 	game = games[key]
 	if request.method == 'GET':
-		return render_template("clue.html", word=game.word, uuid=game.key)
+		letters = random_letters(game.word)
+		return render_template("clue.html", word=game.word, uuid=game.key, letters=letters)
 	elif request.method == 'POST':
 		form_data = request.form
 		print(form_data)
